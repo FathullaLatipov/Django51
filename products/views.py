4c11a011e6bfa51c8b6c10b5d08d379bc5cf0362
FathullaLatipov <fathullaxonlatipov@gmail.com>
2024-06-04 22:00:14 +0500

    test

--- products/views.py
+++ products/views.py
@@ -1,13 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import ListView
 
+from .forms import SearchForm
 from .models import CategoryModel, ProductModel
 
 
-def home_page(request):
+# def home_page(request):
+#
+#     categories = CategoryModel.objects.all()
+#     products = ProductModel.objects.all()
+#     # print -> Laptops,smart,pencil
+#     context = {'categories': categories, 'products': products}
+#     return render(request, template_name='index.html', context=context)
 
-    categories = CategoryModel.objects.all()
-    products = ProductModel.objects.all()
-    # print -> Laptops,smart,pencil
-    context = {'categories': categories, 'products': products}
-    return render(request, template_name='index.html', context=context)
+class HomePage(ListView):
+    template_name = 'index.html'
+    model = ProductModel
+    context_object_name = 'products'
+    form_class = SearchForm
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["categories"] = CategoryModel.objects.all()
+        return context
+
+
+def search(request):
+    # search=='Iphone12'
+    if request.method == 'POST':
+        # <input name='search_product'> <button>
+        get_product = request.POST.get('search_product')  #Iphone12 Iphone13
+        try:
+            exact_product = ProductModel.objects.get(title__icontains=get_product)
+            return redirect(f'/products/{exact_product.id}')
+        except:
+            return redirect('/')
+
+
+# Iphone12 -> Iphone12,price,
+def product_page(request, id):
+    product = ProductModel.objects.get(id=id)
+    context = {'product': product}
+    return render(request, template_name='single-product.html',
+                  context=context)
--- shop/urls.py
+++ shop/urls.py
@@ -4,12 +4,14 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-from products.views import home_page
+from products.views import HomePage, product_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_page),
+    path('', HomePage.as_view()),
+    path('products/<int:id>', product_page)
 ]
+
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
