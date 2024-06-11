1130385d70d3f228035866a4ea55e78bb86f205e
FathullaLatipov <fathullaxonlatipov@gmail.com>
2024-06-11 20:01:13 +0500

    signup

--- shop/urls.py
+++ shop/urls.py
@@ -5,12 +5,18 @@ from django.contrib import admin
 from django.urls import path
 
 from products.views import HomePage, product_page, search
+# ИМПОРТ!!
+from users.views import register_view, login_view, profile_view, logout_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomePage.as_view()),
     path('search', search),
     path('products/<int:id>', product_page),
+    path('signup', register_view),
+    path('login', login_view),
+    path('profile', profile_view),
+    path('logout', logout_view)
 ]
 
 
